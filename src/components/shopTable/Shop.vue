b263d1f828fc4df97fa826dda82e9da810e7e74d
yxr <1662816837@qq.com>
2021-01-21 22:52:37 +0800

    sku回显维护  走之前备份

--- src/components/shopTable/Shop.vue
+++ src/components/shopTable/Shop.vue
@@ -98,6 +98,7 @@
 
           <template slot-scope="scope">
             <el-button type="success" icon="el-icon-edit" @click="toUpdateTable(scope.row.id)" circle></el-button>
+            <el-button type="success" @click="upSkuTable(scope.row)">属性值维护</el-button>
             <!--<el-button type="info" @click="deleteBrand(scope.row.id)" icon="el-icon-delete" circle></el-button>-->
           </template>
         </el-table-column>
@@ -166,11 +167,26 @@
         </el-form-item>
 
 
-          <el-button @click="updateTableData = false">取 消</el-button>
-          <el-button type="primary" @click="updateForm">确 定</el-button>
+        <el-button @click="updateTableData = false">取 消</el-button>
+        <el-button type="primary" @click="updateForm">确 定</el-button>
 
       </el-form>
     </el-dialog>
+    <!----------------------------------------SKU修改模板------------------------------------------->
+    <el-dialog title="商品信息" :visible.sync="SkuDataTable">
+
+      <el-form :model="productForm2" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="分类" prop="typeId">
+          <!--  改变 获取属性数据  -->
+          <el-select v-model="productForm2.typeId" placeholder="请选择" @change="getAttrData">
+            <el-option v-for="item in TypeDatas" :key="item.id" :label="item.name" :value="item.id"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+
+    </el-dialog>
+
+
   </div>
 </template>
 
@@ -187,23 +203,28 @@
         TableData: [],
         //类型数据
         types: [],
+        TypeDatas: [],
+        typeName: "",
+        productForm2: {},
         //品牌数据
         brandData: [],
         //修改表格数据的模板  默认关闭 false
         updateTableData: false,
+        //sku修改的模板
+        SkuDataTable: false,
         //修改商品表格的表单数据
-        updateData:{
-          name:"",
-          title:"",
-          productdecs:"",
-          price:0,
-          stocks:0,
-          sortNum:0,
-          imgPath:"",
-          bandId:"",
-          imgPath:0,
-          typeId:0,
-          id:0
+        updateData: {
+          name: "",
+          title: "",
+          productdecs: "",
+          price: 0,
+          stocks: 0,
+          sortNum: 0,
+          imgPath: "",
+          bandId: "",
+          imgPath: 0,
+          typeId: 0,
+          id: 0
         },
         //分页的参数
         count: 0,
@@ -237,6 +258,15 @@
       queryTypeData: function () {
         this.$ajax.get("http://localhost:8080/GoodsController/getData").then(res => {
           this.types = res.data.data;  // 把请求的数据  赋给全局
+          this.getTypeDatas()
+          //遍历所有的子节点
+          for (var i = 0; i < this.TypeDatas.length; i++) {
+            //定义的全局属性 用来存储我们拼接的底层信息
+            this.typeName = "";
+            //处理子节点的name属性
+            this.chandleName(this.TypeDatas[i]);
+            this.TypeDatas[i].name = this.typeName.split("/").reverse().join("/").substring(0, this.typeName.length - 1);
+          }
         }).catch(err => console.log(err));
       },
       //循环在表格展示我们的
@@ -265,16 +295,16 @@
       //修改商品表格数据的方法
       toUpdateTable: function (id) {
         //单行查询我们的数据  回显到修改的弹框中
-        this.$ajax.post("http://localhost:8080/ShopController/queryDataById?id="+id).then(res => {
+        this.$ajax.post("http://localhost:8080/ShopController/queryDataById?id=" + id).then(res => {
           this.updateData = res.data.data;  // 把请求的数据  赋给全局
           console.log(res)
         }).catch(err => console.log(err));
         this.updateTableData = true;
       },
-      uploadImage:function (response, file, fileList) {
+      uploadImage: function (response, file, fileList) {
         this.updateData.imgPath = response.data;
       },
-      updateForm:function () {
+      updateForm: function () {
         console.log(this.updateData)
         //修改的提交
         this.$ajax.post("http://localhost:8080/ShopController/updataData", this.$qs.stringify(this.updateData)).then(res => {
@@ -282,6 +312,47 @@
           this.updateTableData = false;
           this.queryTableData(1);
         }).catch(err => console.log(err));
+      },
+      upSkuTable: function (row) {
+        this.SkuDataTable = true;
+      },
+      //这是一个查询我们type表的一个方法
+      getAttrData: function () {
+
+      },
+      getTypeDatas() {
+        for (var i = 0; i < this.types.length; i++) {
+          var nnn = this.types[i];
+          //判断所有底层节点 并且赋值到下拉框
+          this.isParent(nnn)
+        }
+        console.log(this.TypeDatas)
+      },
+      isParent: function (datas) {
+        var ccc = true;
+        for (var j = 0; j < this.types.length; j++) {
+          if (datas.id == this.types[j].pid) {
+            ccc = false;
+            break;
+          }
+        }
+        if (ccc == true) {
+          this.TypeDatas.push(datas)
+        }
+      },
+      chandleName: function (node) {
+        if (node.pid != 0) {
+          this.typeName += "/" + node.name;
+          //判断我们传过来的值存不存在父节点
+          for (var i = 0; i < this.types.length; i++) {
+            if (node.pid == this.types[i].id) {
+              this.chandleName(this.types[i]);
+              break;
+            }
+          }
+        } else {
+          this.typeName += "/" + node.name;
+        }
       }
 
 
